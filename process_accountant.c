a0a4779118aa3b00945c6a658a5dc63fa55e1274
Michal Schmidt <mschmidt@redhat.com>
2013-05-21 17:53:03 +0200

    pa: move non-trivial functions to from .h to .c

--- process_accountant.c
+++ process_accountant.c
@@ -39,6 +39,51 @@ void pa_dump(struct process_accountant *pa)
 	printf("-\n");
 }
 
+static struct process *__search_process(struct process_accountant *pa,
+                                               pid_t pid,
+                                               struct rb_node **pparent,
+					       struct rb_node ***plink)
+{
+	struct rb_node **p = &pa->processes.rb_node;
+	struct rb_node *parent = NULL;
+	struct process *process;
+
+	while (*p) {
+		parent = *p;
+		process = rb_entry(parent, struct process, rb_node);
+
+		if (pid < process->pid)
+			p = &(*p)->rb_left;
+		else if (pid > process->pid)
+			p = &(*p)->rb_right;
+		else
+			return process;
+	}
+
+	*pparent = parent;
+	*plink = p;
+
+	return NULL;
+}
+
+void pa_account_latency(struct process_accountant *pa, pid_t pid,
+                        const char comm[16], uint64_t delay,
+                        struct back_trace *bt)
+{
+	struct process *process;
+	struct rb_node *parent;
+	struct rb_node **link;
+
+	process = __search_process(pa, pid, &parent, &link);
+	if (!process) {
+		process = process_new(pid, comm);
+		rb_link_node(&process->rb_node, parent, link);
+		rb_insert_color(&process->rb_node, &pa->processes);
+	}
+	process_suffer_latency(process, delay, bt);
+}
+
+
 void pa_init(struct process_accountant *pa)
 {
 	pa->processes = RB_ROOT;
--- process_accountant.h
+++ process_accountant.h
@@ -17,49 +17,8 @@ void pa_fini(struct process_accountant *pa);
 void pa_summarize(struct process_accountant *pa);
 void pa_dump(struct process_accountant *pa);
 void pa_clear_all(struct process_accountant *pa);
-
-static inline struct process *__search_process(struct process_accountant *pa,
-                                               pid_t pid,
-                                               struct rb_node **pparent,
-					       struct rb_node ***plink)
-{
-	struct rb_node **p = &pa->processes.rb_node;
-	struct rb_node *parent = NULL;
-	struct process *process;
-
-	while (*p) {
-		parent = *p;
-		process = rb_entry(parent, struct process, rb_node);
-
-		if (pid < process->pid)
-			p = &(*p)->rb_left;
-		else if (pid > process->pid)
-			p = &(*p)->rb_right;
-		else
-			return process;
-	}
-
-	*pparent = parent;
-	*plink = p;
-
-	return NULL;
-}
-
-static inline void pa_account_latency(struct process_accountant *pa, pid_t pid,
-                                      const char comm[16], uint64_t delay,
-                                      struct back_trace *bt)
-{
-	struct process *process;
-	struct rb_node *parent;
-	struct rb_node **link;
-
-	process = __search_process(pa, pid, &parent, &link);
-	if (!process) {
-		process = process_new(pid, comm);
-		rb_link_node(&process->rb_node, parent, link);
-		rb_insert_color(&process->rb_node, &pa->processes);
-	}
-	process_suffer_latency(process, delay, bt);
-}
+void pa_account_latency(struct process_accountant *pa, pid_t pid,
+                        const char comm[16], uint64_t delay,
+                        struct back_trace *bt);
 
 #endif
