b9fd4da92aca4c8b8964316403b75c6fa0593319
Michal Schmidt <mschmidt@redhat.com>
2013-05-30 11:41:24 +0200

    binary search in sym_translator

--- sym_translator.c
+++ sym_translator.c
@@ -4,6 +4,7 @@
  * License: GPLv2
  */
 #include <sys/types.h>
+#include <assert.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
@@ -17,7 +18,12 @@ struct symbol {
 	char *name;
 };
 
+/* tree of symbols, only used during kallsyms parsing */
 static struct rb_root addr2fun = RB_ROOT;
+/* after parsing, two simple arrays suffice */
+static unsigned long *addr_array;  /* sorted for binary search */
+static char **name_array;
+static unsigned n_symbols;
 
 static struct symbol *__insert_symbol(unsigned long addr, struct symbol *s)
 {
@@ -52,8 +58,25 @@ static struct symbol *insert_symbol(unsigned long addr, struct symbol *s)
 		goto out;
 	}
 	rb_insert_color(&s->rb_node, &addr2fun);
+	n_symbols++;
 out:
 	return ret;
+}
+
+static void delete_tree(struct rb_node *n)
+{
+	struct symbol *s;
+	if (!n)
+		return;
+	delete_tree(n->rb_left);
+	delete_tree(n->rb_right);
+	s = rb_entry(n, struct symbol, rb_node);
+
+	/* only delete names here if the array hasn't taken ownership of them yet */
+	if (!name_array)
+		free(s->name);
+
+	free(s);
 }
 
 static int parse_kallsyms(void)
@@ -66,6 +89,7 @@ static int parse_kallsyms(void)
 	char type;
 	char *name;
 	struct symbol *s;
+	int r = 0;
 
 	f = fopen("/proc/kallsyms", "re");
 	if (!f) {
@@ -83,55 +107,96 @@ static int parse_kallsyms(void)
 		}
 
 		s = malloc(sizeof(struct symbol));
+		if (!s) {
+			perror("Allocating memory for symbol");
+			delete_tree(addr2fun.rb_node);
+			addr2fun = RB_ROOT;
+			r = -ENOMEM;
+			goto out;
+		}
 		s->addr = addr;
 		s->name = name;
 		insert_symbol(addr, s);
 	}
-	if (line)
-		free(line);
+
+out:
+	free(line);
 	fclose(f);
+	return r;
+}
+
+static int build_arrays(void)
+{
+	struct rb_node *node;
+	unsigned i;
+
+	addr_array = malloc(n_symbols * sizeof(unsigned long));
+	if (!addr_array)
+		return -ENOMEM;
+
+	name_array = malloc(n_symbols * sizeof(char*));
+	if (!name_array) {
+		free(addr_array);
+		addr_array = NULL;
+		return -ENOMEM;
+	}
+
+	i = 0;
+	for (node = rb_first(&addr2fun); node; node = rb_next(node)) {
+		struct symbol *symbol = rb_entry(node, struct symbol, rb_node);
+
+		addr_array[i] = symbol->addr;
+		name_array[i] = symbol->name; /* the array takes ownership of name */
+		i++;
+	}
+	assert(i == n_symbols);
+
+	/* the tree is not needed anymore */
+	delete_tree(addr2fun.rb_node);
+	addr2fun = RB_ROOT;
+
 	return 0;
 }
 
-struct symbol *lookup_lower_bound(unsigned long ip)
+void delete_arrays(void)
 {
-	struct rb_node *n = addr2fun.rb_node;
-	struct symbol *symbol, *lower_bound = NULL;
+	unsigned i;
 
-	while (n) {
-		symbol = rb_entry(n, struct symbol, rb_node);
+	for (i = 0; i< n_symbols; i++)
+		free(name_array[i]);
+	free(name_array);
+	name_array = NULL;
 
-		if (ip < symbol->addr)
-			n = n->rb_left;
-		else if (ip > symbol->addr) {
-			n = n->rb_right;
-			lower_bound = symbol;
-		}
-		else
-			return symbol;
-	}
-	return lower_bound;
-}
-
-static void delete_symbols(struct rb_node *n)
-{
-	struct symbol *s;
-	if (!n)
-		return;
-	delete_symbols(n->rb_left);
-	delete_symbols(n->rb_right);
-	s = rb_entry(n, struct symbol, rb_node);
-	free(s->name);
-	free(s);
+	free(addr_array);
+	addr_array = NULL;
 }
 
 const char *sym_translator_lookup(unsigned long ip)
 {
-	struct symbol *s = lookup_lower_bound(ip);
-	if (s)
-		return s->name;
-	else
+	unsigned low, high, middle;
+	if (!addr_array || !name_array || n_symbols == 0)
 		return NULL;
+
+	low = 0;
+	high = n_symbols - 1;
+
+	if (ip < addr_array[low])
+		return NULL;
+
+	if (ip >= addr_array[high])
+		return name_array[high];
+
+	/* Invariant: addr_array[low] <= ip < addr_array[high] */
+
+	while (high - low > 1) {
+		middle = (low + high) / 2;
+		if (addr_array[middle] <= ip)
+			low = middle;
+		else
+			high = middle;
+	}
+
+	return name_array[low];
 }
 
 /*void sym_translator_dump(void)
@@ -146,10 +211,17 @@ const char *sym_translator_lookup(unsigned long ip)
 
 int sym_translator_init(void)
 {
-	return parse_kallsyms();
+	int r;
+
+	r = parse_kallsyms();
+	if (r)
+		return r;
+
+	return build_arrays();
 }
 
 void sym_translator_fini(void)
 {
-	delete_symbols(addr2fun.rb_node);
+	delete_tree(addr2fun.rb_node);
+	delete_arrays();
 }
